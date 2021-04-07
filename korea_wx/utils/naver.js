5cdb53d167364f5ad2104601a45928a4012e34d6
tomspter <250176010@qq.com>
2021-04-07 23:15:15 +0800

    push

--- korea_wx/utils/naver.js
+++ korea_wx/utils/naver.js
@@ -3,11 +3,11 @@ const axios = require("axios");
 async function naver({type, query, display, start, sort}) {
     let url = ''
     if (type === 'news') {
-        url = `https://openapi.naver.com/v1/search/news.json?query=${encodeURI(query)}&display=${display}&start=${start}&sort=${sort}`
+        url = `https://openapi.naver.com/v1/search/news.json?query=${encodeURI(query)}&display=${display}&start=${start}&sort=sim`
     } else if (type === 'book') {
         url = `https://openapi.naver.com/v1/search/book.json?query=${encodeURI(query)}&display=${display}&start=${start}`
     } else if (type=== 'movie'){
-        url = `https://openapi.naver.com/v1/search/book.json?query=${encodeURI(query)}&display=${display}&start=${start}&country=KR`
+        url = `https://openapi.naver.com/v1/search/movie.json?query=${encodeURI(query)}&display=${display}&start=${start}&country=KR`
     }
     return await axios.get(url, {
         headers: {
